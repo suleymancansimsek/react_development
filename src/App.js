d53ad63c4ece5aae29da5fe5fa11e194358df92c
suleymancansimsek <suleymancansimsek@gmail.com>
2023-01-29 14:24:12 +0300

    creating compenent

--- src/App.js
+++ src/App.js
@@ -1,22 +1,63 @@
+import { useEffect} from "react";
+
 import './App.css';
+import SearchIcon from './search.svg';
+const API_URL = 'http://www.omdbapi.com?apikey=659e5f11';
 
-const Person = (props) =>{
-  return(
-    <>
-    <h1>Name: {props.name}</h1>
-    <h1>LastName: {props.lastName}</h1>
-    <h2>Age: 30</h2>
-    </>
-  )
+const movie1 = {
+    "Title": "Spiderman",
+    "Year": "2010",
+    "imdbID": "tt1785572",
+    "Type": "movie",
+    "Poster": "N/A"
 }
+
 const App = () => {
-  return (
-      <div className="App">
-      <Person name = {'Suleyman'} lastName = {'Simsek'} / >
-      <Person name = {'Matt'} lastName = {'Damon'} / >
-      <Person name = {'Nicholas'} lastName = {'Blabla'} / >
-    </div>
-  );
+
+    const searchMovies = async (title) => {
+        const response = await fetch(`${API_URL}&s=${title}`);
+        const data = await response.json();
+        console.log(data.Search);
+    }
+
+    useEffect( () => {
+        searchMovies('spiderman');
+    },[]);
+
+    return (
+        <div className="app">
+            <h1>MovieLand</h1>
+
+            <div className="search">
+                <input
+                placeholder="Search for movies"
+                value="deneme"
+                onChange={() => {}}
+                />
+                <img
+                    src={SearchIcon}
+                    alt="search"
+                    onClick={() => {}}
+                 />
+            </div>
+
+            <div className="container">
+                <div className="movie">
+                    <div>
+                        <p>{movie1.Year}</p>
+                    </div>
+                    <div>
+                    <img
+                    src={movie1.Poster !== 'N/A' ? movie1.Poster : 'https://via.placeholder.com/400'}
+                    alt={movie1.title}
+                    onClick={() => {}}
+                    /> 
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 }
 
+//659e5f11
 export default App;
